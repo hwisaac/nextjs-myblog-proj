f2b21b8bedce5c85efdb066683ec58ea4c6138d8
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-04-26 00:07:01 +0900

    init

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+type Props = {};
+
+export default function SideBar({}: Props) {
+  return <div>SideBar</div>;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  theme: {},
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ['var(--font-josefin)'],
+        serif: ['var(--font-source-serif-pro)'],
+      },
+      colors: {
+        primary: '#4A4870',
+        fontColor: '#586171',
+        blue: '#4A4870',
+        red: '#FF4B60',
+        white: '#FFFFFF',
+        gray: '#9A9FA9',
+        twitter: '#1DA1F1',
+        facebook: '#165BEA',
+        instargram: '#EF00A1',
+        yellow: '#FAC469',
+        skyBlue: '#65EBE7',
       },
     },
   },
   plugins: [],
-}
+};
