6ce6c4a438c64dfa8917adb3514302dbbabc8e15
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-05-04 23:03:27 +0900

    image 최적화

--- src/components/slider/SlideCard.tsx
+++ src/components/slider/SlideCard.tsx
@@ -10,15 +10,16 @@ export default function SlideCard({ slide }: Props) {
 
   return (
     <div className='w-full h-full relative flex justify-center items-center'>
-      <div className='w-full h-full absolute z-20 bg-orange-500/30'>
+      <div className='w-full h-full absolute z-20'>
         <Image
           src={image}
           alt={alt}
           className='object-cover w-full h-full z-20 saturate-50 brightness-75'
-          sizes='1150px'
+          sizes='400px'
           fill
           priority
           placeholder='blur'
+          quality={50}
         />
       </div>
       <SlideContents tags={tags} date={date} title={title} />
