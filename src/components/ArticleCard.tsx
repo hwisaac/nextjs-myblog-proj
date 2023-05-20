e2bfa57ab9f5461b976014880b7a55dd226e77cf
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-05-20 14:55:28 +0900

    Latest Articles 포스트의 Link 태그

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import UnderlineHeading from './ui/UnderlineHeading';
 import { IPost } from '@/service/post';
 import convertDateFormat from '@/utils/convertDateFormat';
+import Link from 'next/link';
 
 type Props = { post: IPost };
 
@@ -13,7 +14,9 @@ export default function ArticleCard({ post }: Props) {
     <div className='flex h-20 shrink-0 w-full'>
       <div className='w-[25%] shrink-0 h-full bg-slate-200'></div>
       <div className='ml-3 justify-between pb-2 w-[75%]'>
-        <UnderlineHeading text={post.title} size='small' />
+        <Link href={`/posts/${post.slug}`}>
+          <UnderlineHeading text={post.title} size='small' />
+        </Link>
         <span className='block font-serif h-1/4 text-gray-500 text-xs'>
           {convertDateFormat(post.createdAt)}
         </span>
