a11d3c3ffaf004f9f564a1c829b126e4688d807d
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-06-07 14:59:18 +0900

    refactor: tiny change on usePosts hook

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,10 +1,9 @@
 import { IPost } from '@/service/post';
 import createSlug from '@/utils/createSlug';
 import extractDescription from '@/utils/extractDescription';
-import React from 'react';
 import useSWR, { useSWRConfig } from 'swr';
 
-type TCreatePostInput = {
+type TCreatePostParam = {
   form: {
     title: string;
     content: string;
@@ -25,41 +24,40 @@ export default function usePosts() {
   const { mutate: globalMutate } = useSWRConfig();
 
   // 서버에 새로운 post 를 업데이트하기
-  // tempPost
-  const createPost = async ({ form, tags = [], file }: TCreatePostInput) => {
+  const createPost = async ({ form, tags = [], file }: TCreatePostParam) => {
     const { title, content } = form;
 
     if (!posts) return console.log('posts 가 없습니다!');
 
+    // 임시 데이터 tempPost
     const tempPost: IPost = {
       title,
-      slug: '',
+      slug: `${createSlug(title)}-${new Date().getTime()}`,
       postImage: null,
       description: extractDescription(content, 150),
       tags,
-      postId: '',
+      postId: `temp-${new Date().getTime()}`,
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString(),
       commentsLength: 0,
     };
-
+    // posts 목록
     const newPosts: IPost[] = [tempPost, ...posts];
 
-    console.log('newPosts > ', newPosts);
-
+    // addPost 함수[remote mutate]로 서버에 데이터 보내기 + ui 즉각 업데이트
     return mutate(addPost({ form, tags, file }), {
-      optimisticData: newPosts, // 즉시 해당 데이터로 업데이트
-      populateCache: false, // remote mutate 결과를 캐시에 기록하지 않기
-      revalidate: false, // 비동기 데이터가 업데이트돼도 캐시 유효성 검사하지 말기
+      optimisticData: newPosts, // 데이터보낸 동안 즉시 가짜데이터로 ui를 업데이트
+      populateCache: false, // remote mutate 리턴값을를 캐시에 기록하지 않기.
+      revalidate: false, // 비동기 데이터가 업데이트돼도 캐시 유효성 검사하지 말기(=내가 만든 데이터를 믿는다)
       rollbackOnError: true, // remote mutate 과정에서 에러가 나면 캐시를 롤백시키기
-    }).then(() => globalMutate('/api/posts'));
+    }).then(() => globalMutate('/api/posts')); // 서버상 posts를 refetch해서 캐시 업데이트하기
   };
 
   return { posts, isLoading, error, createPost };
 }
 
 // 서버에 데이터를 보내는 함수
-function addPost({ form, tags, file }: TCreatePostInput) {
+function addPost({ form, tags, file }: TCreatePostParam) {
   const { title, content } = form;
   const formData = new FormData();
   if (!file) throw Error('file 이 없습니다.');
@@ -70,5 +68,9 @@ function addPost({ form, tags, file }: TCreatePostInput) {
   return fetch('/api/posts', {
     method: 'POST',
     body: formData,
-  }).then((res) => res.json());
+  }).then(async (res) => {
+    const x = await res.json();
+    console.log(x);
+    return x;
+  });
 }
